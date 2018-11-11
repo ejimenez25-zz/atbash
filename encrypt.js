a5f0bdfe409b0800a953bfa3c15130a66c27e066
ejimenez25 <ejimenez25@csu.fullerton.edu>
2018-11-10 18:06:48 -0800

    code clean up for debugging

--- encrypt.js
+++ encrypt.js
@@ -1,42 +1,33 @@
-// JS program to implement AtBash cipher
-  
-//Object that holds key:value pair
+//JS program that creates an encrypted message using the atbash cipher
+
+//The cipher stored in an object
 let atBashCipher = {
-  'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V', 
-        'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q', 
-        'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L', 
-        'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G', 
-        'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B', 'Z' : 'A'
-} 
+  'A' : 'Z', 'B' : 'Y', 'C' : 'X', 'D' : 'W', 'E' : 'V',
+  'F' : 'U', 'G' : 'T', 'H' : 'S', 'I' : 'R', 'J' : 'Q',
+  'K' : 'P', 'L' : 'O', 'M' : 'N', 'N' : 'M', 'O' : 'L',
+  'P' : 'K', 'Q' : 'J', 'R' : 'I', 'S' : 'H', 'T' : 'G',
+  'U' : 'F', 'V' : 'E', 'W' : 'D', 'X' : 'C', 'Y' : 'B',
+  'Z' : 'A'
+}
 
+//This variable is meant to grab and store the text values
+let message = document.getElementById('text');
 
-let message = document.getElementByID('text');
-
-//declaring a function that takes the message
+//Function that iterates though the function 'message' variable
 function atBash(message){
     //Create an empty variable that will store the iteration
-    let cipher = ''; 
+    let cipher = '';
     //for in loops though properties in an object in this case the user message
-    for (let letter in message){
+    for (let letter in message){
       //Conditional statement that adds a letter to 'cipher' if it isn't a space
+      //from the cipher object
       if(letter != ''){
         cipher += atBashCipher[letter];
-        //Conditional statement that adds 
+        //Conditional statement that adds a space if here is a space
       } else {
         cipher += '';
-      }
+        }
+      //Display the result after the iteration
       return cipher;
-    } 
- 
-  
-//Function that makes the program run. 
-function encrypt(){
-  message = 'GEEKS FOR GEEKS';
-  console.log(atBash(message.upperCase()));
-  
-  message = 'TVVPH ULI TVVPH';
-  console.log(atBash(message.upperCase()));
-} 
-  
-  
-  
+    }
+  }
